7df77c1c35d73b8ef5c4f5b263f885c8d238613b
QuyDang1209 <dangphuocquy1996@gmail.com>
2024-05-29 17:21:17 +0700

    update

--- src/main/java/com/cg/minitest1module4_ajax/controller/TypeRestController.java
+++ src/main/java/com/cg/minitest1module4_ajax/controller/TypeRestController.java
@@ -1,4 +1,23 @@
 package com.cg.minitest1module4_ajax.controller;
 
+import com.cg.minitest1module4_ajax.model.Type;
+import com.cg.minitest1module4_ajax.repository.ITypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/type")
+@CrossOrigin("*")
 public class TypeRestController {
+    @Autowired
+    private ITypeRepository typeRepository;
+    @GetMapping
+    public ResponseEntity<Iterable<Type>> listType(){
+        return new ResponseEntity<>(typeRepository.findAll(), HttpStatus.OK);
+    }
 }
